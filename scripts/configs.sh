b0ad4e9a7ea4402c41445528f99781b019ada663
Multiiiiii <multimitamiensaft@gmail.com>
2024-09-07 16:51:41 +0200

    move

--- scripts/configs.sh
+++ scripts/configs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
+cp -rf ~/catppuccin_dwm/configs/.config ~/
+cp -rf ~/catppuccin_dwm/configs/.scripts ~/
+cp -rf ~/catppuccin_dwm/configs/.spotdl ~/
+cp -rf ~/catppuccin_dwm/.xinitrc ~/
+cp -rf ~/catppuccin_dwm/.Xresources ~/
+cp -rf ~/catppuccin_dwm/.bashrc ~/
+cp -rf ~/catppuccin_dwm/.zshrc ~/
+cp -rf ~/catppuccin_dwm/.p10k.zsh ~/
 cp -rf ~/catppuccin_dwm/configs/* ~/
