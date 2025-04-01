38b701c43e5febb408aa941054f241daab7cb2c5
Multiiiiii <149270406+Multiiiiii@users.noreply.github.com>
2025-04-01 09:45:26 +0200

    changed colors

--- dwm/catppuccin.c
+++ dwm/catppuccin.c
@@ -2,6 +2,6 @@ static const char col_gray1[]       = "#1e1e2e";
 static const char col_gray2[]       = "#11111b"; //unfocused border#89b4fa
 static const char col_gray3[]       = "#cdd6f4"; //text
 static const char col_gray4[]       = "#cba6f7";
-static const char col_cyan[]        = "#89b4fa"; //#11111b
+static const char col_cyan[]        = "#45475a"; //#11111b
 
 
