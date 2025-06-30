d3ebd59e86374794ca136b5e4ac048ead3f4daca
Multiiiiii <149270406+Multiiiiii@users.noreply.github.com>
2025-06-30 09:47:22 +0200

    updated keyboard layout posittion

--- scripts/neoboard.sh
+++ scripts/neoboard.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo cp 00-keyboard.conf /etc/X11/xorg.conf.d/
 sudo cp vconsole.conf /etc/
-sudo cp custom /usr/share/X11/xkb/symbols/
+sudo cp custom /usr/share/xkeyboard-config-2/symbols
 
